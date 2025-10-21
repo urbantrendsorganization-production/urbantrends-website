430d6c8102c1037601d44580f9081a8c37adc584
muchemi wamuyu <muchemiedwin68@gmail.com>
2025-10-21 20:47:04 +0300

    cors update

--- urbantrends-backend/server.js
+++ urbantrends-backend/server.js
@@ -2,6 +2,7 @@ import connectDb from "./config/database.js";
 import express from "express";
 import dotenv from "dotenv";
 import serviceRoutes from './src/Routes/ServiceRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -10,11 +11,32 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://developers.urbantrends.dev",
+]
+
+
+
 // middleware
 app.use(express.json());
 
 // connect to database
 connectDb();
+
+app.use (
+    cors ({
+        origin: (origin, callback) =>{
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error ("Not allowed by cors"));
+            }
+        },
+        credentials: true,
+    })
+
+);
 
 // routes init
 app.use('/api', serviceRoutes);
