f99216dbd1197473184a2ada8d0dfe2a8452f909
muchemi wamuyu <muchemiedwin68@gmail.com>
2025-10-30 18:02:19 +0300

    updated file mismatch

--- urbantrends-backend/server.js
+++ urbantrends-backend/server.js
@@ -52,5 +52,5 @@ app.use('/tiers-products', productTiers);
 
 // start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT} an now is good`);
 });
